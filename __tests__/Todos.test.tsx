01361acecd299c7c9f0c5bf6006a19ccf0ebdc53
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-12 19:02:52 -0300

    Should add task on click

--- __tests__/Todos.test.tsx
+++ __tests__/Todos.test.tsx
@@ -1,22 +1,61 @@
+// Importa o componente Todos da página de teste
 import Todos from "@/app/tdd/page";
-import { render, screen } from "@testing-library/react";
+// Importa funções e métodos úteis para realizar testes
+import { render, screen, fireEvent } from "@testing-library/react";
+// Importa userEvent para simular eventos de usuário
+import userEvent from "@testing-library/user-event";
 
+// Descreve o conjunto de testes para o componente Todos
 describe("Todos", () => {
+  // Testa se a mensagem "Minhas tarefas" está sendo exibida corretamente
   it("Should show My tasks message", () => {
+    // Renderiza o componente Todos
     render(<Todos />);
-    const myElement = screen.getByText("Minhas tarefas");
+    // Seleciona o elemento que contém o texto "Minhas tarefas"
+    const myElement = screen.getByText(/Minhas tarefas/i);
+    // Verifica se o elemento está no documento (DOM)
     expect(myElement).toBeInTheDocument();
   });
 
+  // Testa se o campo de entrada para adicionar tarefas está sendo exibido corretamente
   it("Should show task input", () => {
+    // Renderiza o componente Todos
     render(<Todos />);
-    const input = screen.getByPlaceholderText("Digite o nome da tarefa");
+    // Seleciona o campo de entrada pelo atributo placeholder
+    const input = screen.getByPlaceholderText(/Digite o nome da tarefa/i);
+    // Verifica se o campo de entrada está no documento (DOM)
     expect(input).toBeInTheDocument();
   });
 
+  // Testa se o botão de adicionar tarefa está sendo exibido corretamente
   it("Should show add button", () => {
+    // Renderiza o componente Todos
     render(<Todos />);
-    const button = screen.getByRole("button", { name: "Adicionar tarefa" });
+    // Seleciona o botão pelo papel (role) e pelo nome
+    const button = screen.getByRole("button", { name: /Adicionar tarefa/i });
+    // Verifica se o botão está no documento (DOM)
     expect(button).toBeInTheDocument();
+  });
+
+  // Testa se uma tarefa é adicionada corretamente ao clicar no botão de adicionar
+  it("Should add task on click", async () => {
+    // Renderiza o componente Todos
+    render(<Todos />);
+    // Seleciona o campo de entrada pelo atributo placeholder
+    const input = screen.getByPlaceholderText(/Digite o nome da tarefa/i);
+    // Define o título da tarefa
+    const taskTitle = "Nova tarefa";
+    // Simula a digitação do título da tarefa no campo de entrada
+    await userEvent.type(input, taskTitle);
+    // Verifica se o valor digitado está sendo exibido corretamente no campo de entrada
+    screen.getByDisplayValue(taskTitle);
+    // Seleciona o botão de adicionar tarefa pelo papel (role) e pelo nome
+    const button = screen.getByRole("button", { name: /Adicionar tarefa/i });
+    // Simula o clique no botão de adicionar tarefa
+    await userEvent.click(button);
+    // Verifica se o campo de entrada está vazio após adicionar a tarefa
+    screen.getByPlaceholderText(/Digite o nome da tarefa/i);
+    // Verifica se o título da tarefa adicionada está sendo exibido corretamente
+    screen.getByText(taskTitle);
   });
 });
--- src/app/tdd/page.tsx
+++ src/app/tdd/page.tsx
@@ -1,20 +1,61 @@
-"use client"
-
+"use client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { TodoProps } from "@/interfaces/todos";
+import { v4 as uuid } from "uuid";
 import { ButtonComponent } from "@/components/Button";
 
 export default function Todos() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    resetField,
+    setFocus,
+  } = useForm<{ title: string }>();
+
+  const [todos, setTodos] = useState<TodoProps[]>([]);
+
+  const handleAddTasks = (data: { title: string }) => {
+    setTodos((prev) => [
+      ...prev,
+      { title: data.title, status: false, id: uuid() },
+    ]);
+    resetField("title");
+    setFocus("title");
+  };
+
   return (
-    <div className="w-screen h-screen bg-gray-950 grid place-content-center">
-      <div className="p-4 bg-slate-500 rounded-2xl">
+    <div className="w-screen min-h-screen bg-gray-950 flex justify-center items-start pt-10">
+      <div className="p-4 bg-slate-500 rounded-2xl m-10">
         <h1 className="text-center mb-4">Minhas tarefas</h1>
-       <div className="flex gap-5">
-       <input
-          type="text"
-          placeholder="Digite o nome da tarefa"
-          className="rounded-lg p-3 min-w-96 bg-gray-800 text-gray-100 outline-none border"
-        />
-        <ButtonComponent onClick={()=> console.log(2)} value="Adicionar tarefa"/>
-       </div>
+        <div className="flex gap-5">
+          <input
+            type="text"
+            {...register("title", { required: true })}
+            placeholder="Digite o nome da tarefa"
+            className="rounded-lg p-3 min-w-96 bg-gray-800 text-gray-100 outline-none border"
+          />
+          <ButtonComponent
+            onClick={() => handleSubmit(handleAddTasks)()}
+            value="Adicionar tarefa"
+          />
+        </div>
+        <ul className="flex flex-col gap-3 mt-4">
+          {todos.map((todo) => (
+            <div
+              className="w-full border rounded flex justify-between items-center p-4 "
+              key={todo.id}
+            >
+              <li className="">{todo.title}</li>
+              <span
+                className={`p-1 w-6 h-6 rounded-full ${
+                  todo.status ? "bg-green-500" : "bg-red-600"
+                } `}
+              ></span>
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   );
